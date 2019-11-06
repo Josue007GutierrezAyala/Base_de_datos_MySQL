e3207616c4ee56b27d05e06550f9e903e04684df
Alumno <Alumno@PC26>
2019-11-06 12:30:37 -0600

    Estoy arto

---
+++ WpfApp1/WpfApp1/Principal.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using SQLite;
+using WpfApp1.Clase;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Lógica de interacción para Principal.xaml
+    /// </summary>
+   
+    public partial class Principal : Window
+    {
+        List<Contactos> contactos;
+        public Principal()
+        {
+            InitializeComponent();
+            contactos = new List<Contactos>();
+            LeerBaseDatos();
+        }
+        void LeerBaseDatos()
+        {
+            using (SQLiteConnection conn = new SQLite.SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Contactos>();
+                contactos = (conn.Table<Contactos>().ToList()).OrderBy(c => c.Nombre).ToList();
+                if (contactos !=null)
+                {
+                    lvContactos.ItemsSource = contactos;
+                }
+            }
+        }
+
+        private void BtnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            WpfApp1.MainWindow form = new WpfApp1.MainWindow();
+            form.ShowDialog();
+        }
+    }
+}
